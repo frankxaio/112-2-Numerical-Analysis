15fb0735f085fbbf01e529f7592bae4a3b248f6b
Xiao <frankxaio@gmail.com>
2024-04-02 11:55:26 +0800

    update

---
+++ Homework 5/HW5.m
@@ -0,0 +1,130 @@
+%% 1.a) 圖形法
+% x_lin = linspace(-1000, 1000, 1000000);
+% y = x.^3 - 6*x.^2 + 11*x - 6.1;
+syms x;
+f = @(x) x^3 - 6*x^2 + 11*x - 6.1;
+roots_gold = double(solve(f,x));
+hold on;
+fplot(f, 'LineWidth', 1.5);
+plot(roots_gold, 0, 'ro', 'MarkerSize', 10, 'LineWidth', 2);
+legend('f(x)', 'roots');
+hold off;
+grid on; xlabel('x'); ylabel('f(x)');
+
+%% 1.b) 牛頓-拉夫森法
+syms x 
+f = @(x) x^3 - 6*x^2 + 11*x - 6.1;
+df = diff(f, x);
+xi = 3.5;
+disp("Newton-Raphson method");
+for i = 1:3
+    xi1 = xi - subs(f,xi)/subs(df, xi);
+    xi = double(xi1); % symbolic to value
+    fprintf('Iteration %d, x = %.6f\n', i, xi);
+end
+error = abs(xi-roots_gold(3))/roots_gold(3)*100;
+fprintf("Error: %f%%\n", error);
+
+%% 1.c) 割線法 
+syms x
+f = @(x) x^3 - 6*x^2 + 11*x - 6.1;
+x0 = 2.5; x1 = 3.5;  
+disp("Secant method");
+for i = 1:3
+    x_new = x1 - subs(f, x1)*(x1-x0)/(subs(f, x0)-subs(f, x1));
+    x_new = double(x_new); % symbolic to value
+    fprintf('Iteration %d, x = %.6f\n', i, x_new);
+    x0 = x1; x1 = x_new; 
+end
+error = abs(x_new-roots_gold(3))/roots_gold(3)*100;
+fprintf("Error: %f%%\n", error);
+
+%% 1.d) 修正割線法
+syms x
+f = @(x) x^3 - 6*x^2 + 11*x - 6.1;
+xi = 3.5; delta = 0.01;
+disp("Modified secant method");
+for i = 1:3 
+    x = xi - delta*xi*subs(f, xi)/(subs(f, xi+delta*xi)-subs(f, xi));  
+    x = double(x); % symbolic to value
+    fprintf('Iteration %d, x = %.6f\n', i, x);
+    xi = x;
+end
+error = abs(x-roots_gold(3))/roots_gold(3)*100;
+fprintf("Error: %f%%\n", error);
+
+%% 1.e) 用 roots 函數求所有根
+disp(roots_gold);
+
+%% 2.a) 圖形法
+syms x;
+f = @(x) 7*sin(x)*exp(-x) - 1;
+
+% roots_gold = double(solve(f,x));
+% roots_gold = (solve(f,x));
+% roots_gold = (fsolve(f,-6));
+
+% 用 fsolve 求無限解中的 15 個根
+roots_gold = zeros(1,15);
+for i = 1:16
+    roots_gold(i) = (fsolve(f,(i-1)*(-1*pi)));
+    if i>= 16
+        roots_gold(i) = (fsolve(f,2));
+    end
+end
+
+
+hold on;
+fplot(f, 'LineWidth', 1.5);
+plot(roots_gold, 0, 'ro', 'MarkerSize', 10, 'LineWidth', 2);
+hold off;
+legend('f(x)', 'roots');
+grid on; xlabel('x'); ylabel('f(x)');
+
+%% 2.b) 牛頓-拉夫森法
+syms x 
+f = @(x) 7*sin(x)*exp(-x) - 1;
+df = diff(f, x);
+xi = 0.3;
+disp("Newton-Raphson method");
+for i = 1:3
+    xi1 = xi - subs(f,xi)/subs(df, xi);
+    xi = double(xi1); % symbolic to value
+    fprintf('Iteration %d, x = %.6f\n', i, xi);
+end
+
+error = abs(xi-roots_gold(1))/roots_gold(1)*100;
+fprintf("Error: %f%%\n", error);
+
+%% 2.c) 割線法 
+syms x
+f = @(x) 7*sin(x)*exp(-x) - 1;
+x0 = 0.5; x1 = 0.4;  
+disp("Secant method");
+for i = 1:3
+    x_new = x1 - subs(f, x1)*(x1-x0)/(subs(f, x0)-subs(f, x1));
+    x_new = double(x_new); % symbolic to value
+    fprintf('Iteration %d, x = %.6f\n', i, x_new);
+    x0 = x1; x1 = x_new; 
+end
+
+error = abs(x_new-roots_gold(1))/roots_gold(1)*100;
+fprintf("Error: %f%%\n", error);
+
+%% 2.d) 修正割線法
+syms x
+f = @(x) 7*sin(x)*exp(-x) - 1;
+xi = 3.5; delta = 0.01;
+disp("Modified secant method");
+for i = 1:3 
+    x = xi - delta*xi*subs(f, xi)/(subs(f, xi+delta*xi)-subs(f, xi));  
+    x = double(x); % symbolic to value
+    fprintf('Iteration %d, x = %.6f\n', i, x);
+    xi = x;
+end
+
+error = abs(x-roots_gold(1))/roots_gold(1)*100;
+fprintf("Error: %f%%\n", error);
+
+%% 2.e) 用 roots 函數求所有根
+disp(roots_gold);
---
+++ Homework 5/Numerical Analysis Homework 5.md
@@ -0,0 +1,134 @@
+# Numerical Analysis Homework 5
+
+網頁版: https://hackmd.io/@Xaio/HkA8utfyA
+
+## 找根的方法
+- 定義 symbolic variable `sys(x)`, `sys x`
+- `solve()` 可以只到 symbolic 的解，`fsolve()` 可以找到 numerical 的解。
+- `fzero()` 與 `fsolve()` 相同，但是 `fzero()` 找不到平滑經過的點，但是 `fsolve()` 可以
+    ![image](https://hackmd.io/_uploads/SJEs0KMkC.png)
+- `roots()` 專門找 polynomials 的解
+
+
+:::spoiler 補充
+- 使用 `diff(y)` 直接對 syms 算微分。積分同理，使用 `int()`。
+:::
+
+
+## 1. Determine the highest real root of $f(x)$
+
+$$f(x) = x^3 -6x^2+11x-6.1$$
+
+### 1.a) Graphically 
+
+![image](https://hackmd.io/_uploads/r1fRrxty0.png)
+
+Highest real root: $3.0467$
+
+### 1.b) Using the Newton-Raphson method (three iterations, $x_i= 3.5$)
+
+```
+Newton-Raphson method
+Iteration 1, x = 3.191304
+Iteration 2, x = 3.068699
+Iteration 3, x = 3.047317
+Error: 0.020882%
+```
+
+Newton-Raphson method 成功找到解
+
+
+### 1.c) Using the secant method (three iterations, $x_{i-1}=2.5$ and $x_i=3.5$).
+
+```
+Secant method
+Iteration 1, x = 4.288889
+Iteration 2, x = 5.256667
+Iteration 3, x = 6.655075
+Error: 118.436904%
+```
+
+Secant method 發散
+
+
+### 1.d) Using the modified secant method (five iterations, $x_i = 3.5$ ,$\delta= 0.01$ ).
+
+```
+Modified secant method
+Iteration 1, x = 3.199597
+Iteration 2, x = 3.075324
+Iteration 3, x = 3.048818
+Error: 0.070165%
+```
+
+Modified secant method 成功找到解
+
+
+### 1.e) Determine all the roots with MATLAB.
+
+```
+    1.0544
+    1.8990
+    3.0467
+```
+
+
+## 2. Determine the lowest real root of $g(x)$
+
+$$g(x) : 7\sin(x)e^{-x}-1$$
+
+$g(x) < 0$ 有無窮多解，在此的 lowest real root 只討論 $x>0$ 的情形
+
+
+### 1.a) Graphically 
+
+![image](https://hackmd.io/_uploads/BkuNdeFkA.png)
+
+### 2.b) Using the Newton-Raphson method (three iterations, $x_i= 0.3$)
+
+```
+Newton-Raphson method
+Iteration 1, x = 0.144376
+Iteration 2, x = 0.169409
+Iteration 3, x = 0.170179
+Error: 0.000422%
+```
+
+Newton-Raphson method 成功找到解
+
+### 2.c) Using the secant method (three iterations, $x_{i-1}=0.5$ and $x_i=0.4$).
+
+
+```
+Secant method
+Iteration 1, x = 0.797218
+Iteration 2, x = 1.960000
+Iteration 3, x = 2.035892
+Error: 1096.316989%
+```
+
+Secant method 找到非負次小的解 $1.893$，與此解的誤差為 $7.55\%$
+
+
+### 2.d) Using the modified secant method (five iterations, $x_i = 3.5$ ,$\delta= 0.01$ ).
+
+```
+Modified secant method
+Iteration 1, x = -5.682098
+Iteration 2, x = -7.519326
+Iteration 3, x = -6.790712
+Error: 4090.311458%
+```
+
+Modified secant method 找到 $x<0$ 次大的解 $-6.2829$，與此解的誤差為 $8.083\%$
+
+### 2.e) Determine all the roots with MATLAB.
+
+有無窮多解，x 在 [-45, 2] 的範圍內有下列這些解
+
+| roots             | roots             | roots             | roots             |
+|:----------------- |:----------------- |:----------------- |:----------------- |
+| 0.170179993752879 | -3.14772834675799 | -6.28291845844458 | -9.42478948913899 |
+| -12.5663701161672 | -15.7079632894778 | -18.8495559206084 | -21.9911485751688 |
+| -25.1327412287166 | -28.2743338823082 | -31.4159265358979 | -34.5575191894877 |
+| -37.6991118430775 | -40.8407044966673 | -43.9822971502571 | 1.89305902941322  |
